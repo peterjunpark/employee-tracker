102ea91176922b4adbe42743291742798a90bb6d
Peter Park <peterjpark@pm.me>
2023-05-24 16:58:45 -0400

    Add Task classes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const { getTable } = require("./lib/db-queries.js");
+const { ViewTask } = require("./lib/Task.js");
 
 console.log("\x1b[1;32m --- Employee Tracker --- ");
 const questions = [
@@ -8,50 +8,50 @@ const questions = [
     type: "list",
     message: "Choose a task:",
     choices: [
-      // Value format: {task type}@{table}/{by?}
-      { name: "View all departments", value: "view@department" },
-      { name: "View all roles", value: "view@role" },
-      { name: "View all employees", value: "view@employee" },
-      {
-        name: "View employees by department",
-        value: "view@employee@department",
-      },
-      {
-        name: "View employees by manager",
-        value: "view@employee@manager",
-      },
-      {
-        name: "View the total utilized budget of a department",
-        value: "view@department@budget",
-      },
-      new inquirer.Separator(),
-      { name: "Add a department", value: "add@department" },
-      { name: "Add a role", value: "add@role" },
-      { name: "Add an employee", value: "add@employee" },
-      new inquirer.Separator(),
-      { name: "Update an employee's role", value: "update@employee@role" },
-      {
-        name: "Update an employee's manager",
-        value: "update@employee@manager",
-      },
-      new inquirer.Separator(),
-      { name: "Delete a department", value: "delete@department" },
-      { name: "Delete a role", value: "delete@role" },
-      { name: "Delete an employee", value: "delete@employee" },
-      new inquirer.Separator(),
+      { name: "View all departments", value: new ViewTask("department") },
+      { name: "View all roles", value: new ViewTask("role") },
+      { name: "View all employees", value: new ViewTask("employee") },
+      // {
+      //   name: "View employees by department",
+      //   value: "view@employee@department",
+      // },
+      // {
+      //   name: "View employees by manager",
+      //   value: "view@employee@manager",
+      // },
+      // {
+      //   name: "View the total utilized budget of a department",
+      //   value: "view@department@budget",
+      // },
+      // new inquirer.Separator(),
+      // { name: "Add a department", value: "add@department" },
+      // { name: "Add a role", value: "add@role" },
+      // { name: "Add an employee", value: "add@employee" },
+      // new inquirer.Separator(),
+      // { name: "Update an employee's role", value: "update@employee@role" },
+      // {
+      //   name: "Update an employee's manager",
+      //   value: "update@employee@manager",
+      // },
+      // new inquirer.Separator(),
+      // { name: "Delete a department", value: "delete@department" },
+      // { name: "Delete a role", value: "delete@role" },
+      // { name: "Delete an employee", value: "delete@employee" },
+      // new inquirer.Separator(),
     ],
   },
 ];
 
 inquirer.prompt(questions).then(({ task }) => {
-  const [taskType, taskTable, taskColumn] = task.split("@");
-  switch (taskType) {
+  switch (task.type) {
     case "view":
-      getTable(taskTable);
-      //TODO: Implement logic for add, update, delete
+      task.viewTable();
+      break;
     case "add":
     case "update":
     case "delete":
-      console.error("not implemented yet")
+      console.log("not implemented");
+    default:
+      throw new Error("Invalid task type.")
   }
 });
